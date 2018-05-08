d41d47d50e0c79a4ed84baaf01a12c6a06179c0f
U-QA12\user <QA12+user@qa12.trgdev.local>
2018-05-08 11:13:22 +0300

    modified file

--- smena_raskladki.py
+++ smena_raskladki.py
@@ -3,11 +3,14 @@ import slovar
 
 
 slo = slovar.abc
-slovo = str(input('Введите текст: '))
-new_slovo = []
-for bu in slovo:
-    for key in slo:
-        if bu == key:
-            new_slovo.append(slo[key])
-new_slovo = ''.join(new_slovo)
-print(new_slovo)
+while True:
+    slovo = str(input('Введите текст: '))
+    new_slovo = []
+    for bu in slovo:
+        for key in slo:
+            if bu == key:
+                new_slovo.append(slo[key])
+    new_slovo = ''.join(new_slovo)
+    if new_slovo == '':
+        break
+    print(new_slovo)
